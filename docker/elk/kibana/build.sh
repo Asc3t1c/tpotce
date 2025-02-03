5315f5defdb4cdea3c03004dd3a3d7d7803b84e8
nu11secur1ty <venvaropt@gmail.com>
2025-02-03 16:28:58 +0200

    up
    up

--- docker/elk/kibana/build.sh
+++ docker/elk/kibana/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-docker build -t nu11secur1ty/kibana:24.04 .
-docker push nu11secur1ty/kibana:24.04
+docker build -t f0rc3ps/kibana:24.04 .
+docker push f0rc3ps/kibana:24.04
 exit 0;
